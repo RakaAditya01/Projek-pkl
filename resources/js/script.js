c2384466e5cdcf2586b70ef5dbddf480546b8f98
ARIQFADHLURRAHMAN <fadhlurrahmanariq51@gmail.com>
2022-09-28 10:29:39 +0700

    auttofill sesuai user login

---
+++ resources/js/script.js
@@ -0,0 +1,11 @@
+// function isi_otomatis(){
+//     var id = $("#id").val();
+//     $.ajax({
+//         url: 'proses-ajax.php',
+//         data:"id="+id ,
+//     }).success(function (data) {
+//         var json = data,
+//         obj = JSON.parse(json);
+//         $('#nama').val(obj.nama);
+//     });
+// }
