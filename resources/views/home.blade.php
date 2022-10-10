af9bc111db4d81f63ade955014997a678c7018ac
RakaAaditya01 <raka27275@gmail.com>
2022-10-10 14:48:12 +0700

    Perbaikan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>Sarpras - Dashboard</title>
 
     <!-- Custom fonts for this template-->
     <link href="/Template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
